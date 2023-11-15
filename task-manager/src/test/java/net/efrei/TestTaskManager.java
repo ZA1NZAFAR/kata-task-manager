668c1dba36c8cd91ec22ea8f6a0709e4c2a66d38
za1nzafar <zainzafar@outlook.fr>
2023-11-15 15:22:25 +0100

    added test testParsePlus

--- task-manager/src/test/java/net/efrei/TestTaskManager.java
+++ task-manager/src/test/java/net/efrei/TestTaskManager.java
@@ -1,6 +1,29 @@
 package net.efrei;
 
+import jdk.internal.util.xml.impl.Pair;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestTaskManager {
 
+    private TaskManager taskManager;
+
+    @BeforeAll
+    public void setUp() {
+        taskManager = new TaskManager();
+    }
+
+    @Test
+    public void testParsePlus() {
+        Pair result =  taskManager.parse("+ Learn JAVA");
+        Pair expected = new Pair();
+        expected.name = "+";
+        expected.value = "Learn JAVA";
+
+        assertEquals(expected, result);
+    }
 
 }
